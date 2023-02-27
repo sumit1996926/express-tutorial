a6975b0e0fb28edefacb66986eb5f50e39123108
Sumit Yadav <sumit1996926@gmail.com>
2023-02-27 17:32:43 +0530

    hello

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require("express");
-
 const app = express();
 
-app.listen(5000, () => {
-  console.log("serveris listening on port 5000");
+app.get("/", (req, res) => {
+  res.json("./data.js");
 });
-//app.get
-//app.post
-//app.put
-//app.delete
-//app.all
-//app.use
-//app.listen
+
+app.listen(5000, () => {
+  console.log("Server is listening on post 5000");
+});
