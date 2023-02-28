30a266f030377aa18a68990143742dd454fc260f
Sumit Yadav <sumit1996926@gmail.com>
2023-02-28 09:39:57 +0530

    abcd

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+const logger = require("./logger");
+const authorize = require("./authorize");
+// req=> middleware => res
 
+app.use([authorize, logger]);
 app.get("/", (req, res) => {
-  res.json("./data.js");
+  res.send("<h1>Home</h1> <a href='/about'>about</a>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+
+app.get("/api/items", (req, res) => {
+  console.log(req.user);
+  res.send("Items");
 });
 
 app.listen(5000, () => {
-  console.log("Server is listening on post 5000");
+  console.log("Server is listening on port 5000..");
 });
